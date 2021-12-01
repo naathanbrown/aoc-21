86ced5eb41d58d72b70a75fb76c97f048347c1d6
nbrown <Nathan.Brown@frasers.group>
2021-12-01 09:34:19 +0000

    completed part 2

---
+++ day1/sonar-window-sweep.py
@@ -0,0 +1,16 @@
+with open('input-part1.txt') as f:
+    sonar_input = f.read().splitlines() 
+
+sonar_input = list(map(int, sonar_input))
+window_size = 3
+increase_count = 0
+last_depth = -1
+
+for i in range(len(sonar_input) - window_size + 1):
+    depth = sum(sonar_input[i: i + window_size])
+    if (last_depth > 0):
+        if(depth > last_depth):
+            increase_count += 1
+    last_depth = depth
+
+print(increase_count)
