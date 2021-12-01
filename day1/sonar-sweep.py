097cfc714e213204cfc430201644b1df81901719
nbrown <Nathan.Brown@frasers.group>
2021-12-01 09:48:50 +0000

    updated to none

--- day1/sonar-sweep.py
+++ day1/sonar-sweep.py
@@ -4,10 +4,10 @@ with open('input-part1.txt') as f:
 sonar_input = list(map(int, sonar_input))
 
 increase_count = 0
-last_depth = -1
+last_depth = None
 
 for depth in sonar_input:
-    if (last_depth > 0):
+    if (last_depth):
         if(depth > last_depth):
             increase_count += 1
     last_depth = depth
--- day1/sonar-window-sweep.py
+++ day1/sonar-window-sweep.py
@@ -4,11 +4,11 @@ with open('input-part1.txt') as f:
 sonar_input = list(map(int, sonar_input))
 window_size = 3
 increase_count = 0
-last_depth = -1
+last_depth = None
 
 for i in range(len(sonar_input) - window_size + 1):
     depth = sum(sonar_input[i: i + window_size])
-    if (last_depth > 0):
+    if (last_depth):
         if(depth > last_depth):
             increase_count += 1
     last_depth = depth
