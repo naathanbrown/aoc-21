1489336db7baec9d7f73a9b45f6491f2ade51eca
nbrown <Nathan.Brown@frasers.group>
2021-12-04 16:24:40 +0000

    updated part 1

--- day4/bingo.py
+++ day4/bingo.py
@@ -7,21 +7,44 @@ class BingoBoard:
     def SumBoard(self, number):
         score = 0
         for row in self.board:
+            row = [0 if x == -1 else x for x in row]
             score += sum(row)
         return score * number
 
     def CheckBoardAndRemove(self, number):
         for idx, row in enumerate(self.board):
-            row = [x for x in row if x != number]
+            row = [-1 if x == number else x for x in row]
             self.board[idx] = row
-        return self.CheckWinner(number)
+        return self.SumBoard(number) if self.CheckWinner() else self
 
-    def CheckWinner(self, number):
+    def GetColumnNumber(self, row):
+        return self.board.index(row)
+
+    def CheckColumnWin(self):
         for row in self.board:
-            if len(row) == 0:
-                return self.SumBoard(number)
-            else:
-                return self
+            if -1 in row and self.GetColumnNumber(row) == 0:
+                index_list = []
+                for idx, value in enumerate(row):
+                    if value == -1:
+                        index_list.append(idx)
+                for idx in index_list:
+                    if self.board[1][idx] == -1 and self.board[2][idx] == -1 and self.board[3][idx] == -1 and self.board[4][idx] == -1:
+                        return True
+        return False
+
+    def CheckRowWin(self):
+        for row in self.board:
+            if all([ v == -1 for v in row ]):
+                return True
+        return False
+                
+    def CheckWinner(self):
+        if self.CheckRowWin():
+            return True
+        elif self.CheckColumnWin():
+            return True
+        else:
+            return False
 
 def game_loop():
     with open('input.txt') as f:
